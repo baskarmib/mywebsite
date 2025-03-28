2a10b4bceae73fed7df1306a95b2d96d90c39bc9
Baskar Rao <baskarrao.dandlamudi@outlook.com>
2025-03-28 05:51:56 -0400

    Update Index.vue
    Updated changes with experience.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -14,17 +14,17 @@
        Baskar Rao Dandlamudi
       </h1>  
       <p class="subtitle">
-      Auth0 Ambassador, Cloudinary Media Developer Expert, Founding Member JavaScript and Friends
+      Ex Auth0 Ambassador, Founding Member JavaScript and Friends
       </p>   
       <p >
-      With over 14 years of experience developing software applications and designing solutions, he solves complex problems faced by enterprise applications on a day to day basis. 
-      He is a technologist and tries to keep himself updated with latest technology advancements. 
-      In his free time, he blogs and speaks at conferences and code camps sharing his experiences with new technologies. 
-      He organizes JavaScript and Friends Annual Conference, DotNet OpenSource Days and conducts monthly developer meetups on topics based on Angular, React, Vue and JavaScript. 
+      Graduated from Kelley School of Business, Indiana University with Master's in Information Technology Management and has over 17 plus years of experience architecting and developing software applications and designing solutions, he solves complex problems faced by enterprise applications on a day to day basis. 
+      He is a technologist and tries to keep himself updated with latest technology advancements.       
+      He has organized conferences like JavaScript and Friends and DotNetOpenSourceDays during his free time. 
+      In his free time, he blogs and speaks at conferences and code camps sharing his experiences with new technologies and conducts mentoring sessions for early and mid career software engineers.   
       </p>
       <br>
       <p>
-      His interest areas include Cloud Computing, Web Development and Mobile Application Development using Microsoft Azure, Kubernetes, Angular, VueJs and NativeScript.
+      His interest areas include Cloud Computing Microsoft Azure, Kubernetes, Angular, VueJs and NativeScript.
       </p> 
       <br>     
       <p class="subtitle">Certifications and Courses</p>
@@ -36,6 +36,7 @@
       <span class="tag is-info">Introduction to Enterprise Architecture</span> &nbsp;
       <span class="tag is-info">Shaping up with AngularJS</span> &nbsp;
       <span class="tag is-info">Digital Technology and Innovation</span> &nbsp;
+      <span class="tag is-info">Digital and IT Strategy</span> &nbsp;
       </p>
     </div>
   </div>
